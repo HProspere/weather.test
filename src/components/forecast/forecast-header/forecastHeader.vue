6c977fb47316e883eb844b15d35f477f4a485fc8
hprospere <homo.prospere@gmail.com>
2025-09-08 11:39:13 +0300

    forecastHeader component done (without adaptive)

--- src/components/forecast/forecast-header/forecastHeader.vue
+++ src/components/forecast/forecast-header/forecastHeader.vue
@@ -1,11 +1,41 @@
 <script setup lang="ts">
+import { useForecastStore } from '@/stores/forecast.ts';
+import { storeToRefs } from 'pinia';
+import { computed } from "vue";
 
+const forecastStore = useForecastStore();
+const { selectedCity } = storeToRefs(forecastStore);
+
+const dateText = computed(() => new Intl.DateTimeFormat(
+  'ru-RU',
+  {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  }).format(selectedCity.value.daily?.[0].timestamp)
+)
 </script>
 
 <template>
-  Forecast header component
+  <div class="forecast-header">
+    <div class="city-name">Погода в городе {{selectedCity.name}}</div>
+    <div class="date-text">{{dateText}}</div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+.forecast-header {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
 
+  .city-name {
+    @include typo.font-h4;
+  }
+
+  .date-text {
+    @include typo.font-p1-med;
+    color: colors.$font-sub-caption;
+  }
+}
 </style>
