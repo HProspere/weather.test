650c68164dae06ff4a2d57923078b2edd4da67a6
hprospere <homo.prospere@gmail.com>
2025-09-08 11:35:34 +0300

    Routes improved

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,19 +6,26 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      redirect: () => ({ path: '/daily' }),
+      redirect: () => ({ name: 'dailyForecast' }),
     },
     {
       path: '/daily',
       name: 'dailyForecast',
-      component: () => import('../components/forecast/forecast-daily/forecastDaily.vue'),
+      component: () => import('@/components/forecast/forecast-daily/forecastDaily.vue'),
+      meta: { isRoot: true, title: 'Главная' },
     },
     {
       path: '/weekly',
-      name: 'weeklyForecast',
-      component: () => import('../components/forecast/forecast-weekly/forecastWeekly.vue'),
+      name: `weeklyForecast`,
+      component: () => import('@/components/forecast/forecast-weekly/forecastWeekly.vue'),
+      meta: { isRoot: true, title: 'Погода на неделю' },
+    },
+    {
+      path: '/:data(.*)',
+      name: 'NotFound',
+      redirect: () => ({ name: 'dailyForecast' }),
     },
   ],
 })
 
-export default router
+export default router;
