8aab5c806d2de766f842c2e1eb2b53d962098d6e
hprospere <homo.prospere@gmail.com>
2025-09-08 11:38:39 +0300

    forecastDaily component done (without adaptive)

--- src/components/forecast/forecast-daily/forecastDaily.vue
+++ src/components/forecast/forecast-daily/forecastDaily.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-
+import ForecastHeader from '@/components/forecast/forecast-header/forecastHeader.vue';
+import ForecastDailyCurrent from '@/components/forecast/forecast-daily/forecast-daily-current/forecastDailyCurrent.vue';
 </script>
 
 <template>
-  Daily forecast
+  <forecast-header />
+  <forecast-daily-current />
 </template>
 
 <style lang="scss" scoped>
-
+  .forecast-daily {
+    width: 100%;
+    border: 1px solid red;
+    padding: 12px;
+  }
 </style>
